ef36a04086018eb98c6d5945f6fbdc810d8b4494
Ramon1111 <jramoncito7-99@hotmail.com>
2022-05-31 20:37:06 +0900

    my error

--- Assignments/1 html/peerReview/guajoeipnida/polls/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from polls import views
-
-urlpatterns = [
-    path('', views.index, name = 'index'),
-    path('2/', views.if_else, name = 'if_else'),
-    path('3/', views.for_loop, name = 'for_loop'),
-    ]
